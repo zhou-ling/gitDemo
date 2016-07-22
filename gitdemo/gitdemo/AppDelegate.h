b942f2fe8b0647d547948a324c9b21c3aac1d61d
zhouling <zhouling@allinmd.cn>
2016-07-22 14:10:36 +0800

    test

---
+++ gitdemo/gitdemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitdemo
+//
+//  Created by zhouling on 16/7/22.
+//  Copyright © 2016年 allin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitdemo/gitdemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitdemo
+//
+//  Created by zhouling on 16/7/22.
+//  Copyright © 2016年 allin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
